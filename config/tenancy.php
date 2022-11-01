72a59b8a909152ac080ce0a837cc13f40484c414
Robert Hernnandez <rahernande96@gmail.com>
2022-10-31 21:10:33 -0400

    fix

--- config/tenancy.php
+++ config/tenancy.php
@@ -17,8 +17,8 @@ return [
      * Only relevant if you're using the domain or subdomain identification middleware.
      */
     'central_domains' => [
-        'localhost',
-        // 'lesirve.com',
+        'www.lesirve.com',
+        'lesirve.com',
     ],
 
     /**
@@ -167,7 +167,7 @@ return [
         // Stancl\Tenancy\Features\TelescopeTags::class,
         // Stancl\Tenancy\Features\UniversalRoutes::class,
         // Stancl\Tenancy\Features\TenantConfig::class, // https://tenancyforlaravel.com/docs/v3/features/tenant-config
-        Stancl\Tenancy\Features\CrossDomainRedirect::class, // https://tenancyforlaravel.com/docs/v3/features/cross-domain-redirect
+        // Stancl\Tenancy\Features\CrossDomainRedirect::class, // https://tenancyforlaravel.com/docs/v3/features/cross-domain-redirect
     ],
 
     /**
